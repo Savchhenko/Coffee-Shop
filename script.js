fd16c57294e3ff850022b5f03f5445a617efd460
Alexandra Savchenko <svchhh@yandex.ru>
2022-09-23 00:21:34 +0300

    add closing cart/menu/search

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ const navbar = document.querySelector(".navbar");
 const cartItem = document.querySelector(".cart-items-container");
 const searchForm = document.querySelector(".search-form");
 
-// открывает бургер-меню по клику на иконку
+// открывает/закрывает бургер-меню по клику на иконку/скролу
 document.querySelector("#menu-btn").onclick = () => {
     navbar.classList.toggle("active");
+    cartItem.classList.remove("active");
+    searchForm.classList.remove("active");
 };
 
-// открывает корзину по клику на иконку
+// открывает/закрывает корзину по клику на иконку/скролу
 document.querySelector("#cart-btn").onclick = () => {
     cartItem.classList.toggle("active");
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
 };
 
-// открывает инпут с поиском по клику на иконку
+// открывает/закрывает инпут с поиском по клику на иконку/скролу
 document.querySelector("#search-btn").onclick = () => {
     searchForm.classList.toggle("active");
+    navbar.classList.remove("active");
+    cartItem.classList.remove("active");
 };
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+    cartItem.classList.remove("active");
+    searchForm.classList.remove("active");
+}
